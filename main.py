56448b7f0b02b52c210629be52f8fe8d98149205
Matthew Carroll <matthew.j.carroll@outlook.com>
2020-12-11 12:18:49 -0600

    Models notebook and quick change to preprocess FC1 data

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import scipy.io
 import os
 
-DATA_FOLDER_PATH = '/home/matt/eeg_data/Export File'
+DATA_FOLDER_PATH = '/home/matt/eeg_data/power/LNR/evoked'
 
 eeg_data = []
 counter = 0
 for f in os.listdir(DATA_FOLDER_PATH):
     if counter == 1:
         break
+    # elif f == '4NBOA0112_right_Reward.mat':
     eeg_data.append(scipy.io.loadmat(DATA_FOLDER_PATH + "/" + f))
+    print(f)
     counter += 1
 
 clean_print = (eeg_data[0])
-del clean_print['Markers']
-del clean_print['Channels']
-del clean_print['ChannelCount']
-del clean_print['MarkerCount']
-del clean_print['SampleRate']
-del clean_print['SegmentCount']
+# del clean_print['Markers']
+# del clean_print['Channels']
+# del clean_print['ChannelCount']
+# del clean_print['MarkerCount']
+# del clean_print['SampleRate']
+# del clean_print['SegmentCount']
 del clean_print['__header__']
 del clean_print['__version__']
 del clean_print['__globals__']
 
 print(clean_print)
+print(clean_print['POW_evoked'].shape)
--- preprocess.py
+++ preprocess.py
@@ -9,7 +9,7 @@ def make_stationary(dataset):
     '''Returns transformed dataset that is stationary.'''
 
     # remove negatives and make min val 1
-    dataset += 1 + (-1*min(dataset))
+    dataset += 1 + (-1*np.min(dataset))
 
     # boxcox transformation to dynamically remove trends
     dataset, lam = boxcox(dataset)
@@ -21,24 +21,29 @@ def make_stationary(dataset):
 
     return dataset, lam, min(dataset)
 
-DATA_FOLDER_PATH = 'D:\power_out2\LNR\evoked'
-FCz_data = scipy.io.loadmat(DATA_FOLDER_PATH + "/" + "POWevoked_leftnorew_FCz.mat")
+DATA_FOLDER_PATH = '/home/matt/eeg_data/power/LNR/evoked'
+FCz_data = scipy.io.loadmat(DATA_FOLDER_PATH + "/" + "POWevoked_leftnorew_FC1.mat")
 
 
-eeg_data = []
-counter = 0
-for f in os.listdir(DATA_FOLDER_PATH):
+# eeg_data = []
+# counter = 0
+# for f in os.listdir(DATA_FOLDER_PATH):
 
-    if 'FCz' in f:
-        continue
-    else:
-        eeg_data.append(scipy.io.loadmat(DATA_FOLDER_PATH + "/" + f))
+#     if 'FCz' in f:
+#         continue
+#     else:
+#         eeg_data.append(scipy.io.loadmat(DATA_FOLDER_PATH + "/" + f))
         
-    counter += 1
-    if counter == 3:
-        break
+#     counter += 1
+#     if counter == 3:
+#         break
 
 feedback_onset_FCz_theta =  FCz_data['POW_evoked'][:,3:7,626:888]
-current_FCz_theta_compare = feedback_onset_FCz_theta[0,0]
-current_FCz_theta_compare,_,_ = make_stationary(current_FCz_theta_compare)
-train_ssm.train_arma(current_FCz_theta_compare)
+for i in range(feedback_onset_FCz_theta.shape[0]):
+    for j in range(feedback_onset_FCz_theta.shape[1]):
+        current_FCz_theta_compare = feedback_onset_FCz_theta[i,j]
+        current_FCz_theta_compare,_,_ = make_stationary(current_FCz_theta_compare)
+        # np.savetxt("FC1_theta_data.csv", current_FCz_theta_compare, delimiter=",")
+        with open("FC1_theta_data.csv", 'ab') as abc:
+            np.savetxt(abc, current_FCz_theta_compare, delimiter=",")
+# train_ssm.train_arma(current_FCz_theta_compare)
